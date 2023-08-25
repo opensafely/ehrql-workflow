a753fc961512de87788fa6fafeb7c6e48561cf9e
milanwiedemann <milan.wiedemann@gmail.com>
2023-08-25 16:28:50 +0100

    Add has diabetes variable
    This creates a 'has_diabetes' variable that returns TRUE if a patient has a current unresolved diabetes diagnosis.

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -1,5 +1,5 @@
 from ehrql import Dataset, codelist_from_csv
-from ehrql.tables.beta.core import patients
+from ehrql.tables.beta.core import patients, clinical_events
 
 diabetes_codelist = codelist_from_csv(
     "codelists/nhsd-primary-care-domain-refsets-dm_cod.csv", column="code"
@@ -13,6 +13,27 @@ index_date = "2020-03-31"
 
 dataset = Dataset()
 
-dataset.age = patients.age_on(index_date)
+prior_clinical_events = clinical_events.where(
+    clinical_events.date.is_on_or_before(index_date)
+)
 
-dataset.define_population((dataset.age > 18))
+latest_diabetes_event = (
+    prior_clinical_events.where(
+        prior_clinical_events.snomedct_code.is_in(diabetes_codelist)
+    )
+    .sort_by(prior_clinical_events.date)
+    .last_for_patient()
+)
+
+latest_diabetes_resolved_event = (
+    prior_clinical_events.where(
+        prior_clinical_events.snomedct_code.is_in(diabetes_resolved_codelist)
+    )
+    .sort_by(prior_clinical_events.date)
+    .last_for_patient()
+)
+
+dataset.age = patients.age_on(index_date)
+dataset.has_diabetes = latest_diabetes_resolved_event.date < latest_diabetes_event.date
+
+dataset.define_population((dataset.age > 18) & dataset.has_diabetes)
