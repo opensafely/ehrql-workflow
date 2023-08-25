8ca31ea45d207bb2bf5d2d4b87265dd0a8e52bdf
milanwiedemann <milan.wiedemann@gmail.com>
2023-08-25 16:26:10 +0100

    Add diabetes codelists

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -1,5 +1,13 @@
-from ehrql import Dataset
+from ehrql import Dataset, codelist_from_csv
 from ehrql.tables.beta.core import patients
+
+diabetes_codelist = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-dm_cod.csv", column="code"
+)
+
+diabetes_resolved_codelist = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-dmres_cod.csv", column="code"
+)
 
 index_date = "2020-03-31"
 
