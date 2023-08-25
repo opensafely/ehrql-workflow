89a49572bff10671977e9d419d921e789eed62c1
milanwiedemann <milan.wiedemann@gmail.com>
2023-08-25 14:25:59 +0100

    Add diabetes codelists

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -1,11 +1,18 @@
-from ehrql import Dataset
+from ehrql import Dataset, codelist_from_csv
 from ehrql.tables.beta.core import patients
 
-# Create dataset
+diabetes_codelist = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-dm_cod.csv", column="code"
+)
+
+diabetes_resolved_codelist = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-dmres_cod.csv", column="code"
+)
+
+index_date = "2020-03-31"
+
 dataset = Dataset()
 
-# Create date_of_birth variable from patients table
-dataset.date_of_birth = patients.date_of_birth
+dataset.age = patients.age_on(index_date)
 
-# Define study population
-dataset.define_population(patients.date_of_birth.is_on_or_before("1999-12-31"))
+dataset.define_population((dataset.age > 18))
