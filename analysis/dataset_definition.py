a5b1bbac764b9eb647cdb559c1e027c05bcfe7e7
milanwiedemann <milan.wiedemann@gmail.com>
2023-08-25 14:58:29 +0100

    Create dm variables and generate input dummy data

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -1,22 +1,39 @@
 from ehrql import Dataset, codelist_from_csv
 from ehrql.tables.beta.core import patients, clinical_events
 
-# Define codelists
 diabetes_codelist = codelist_from_csv(
-    "codelists/nhsd-primary-care-domain-refsets-dm_cod.csv",
-    column="code"
-    )
+    "codelists/nhsd-primary-care-domain-refsets-dm_cod.csv", column="code"
+)
 
 diabetes_resolved_codelist = codelist_from_csv(
-    "codelists/nhsd-primary-care-domain-refsets-dmres_cod.csv",
-    column="code"
-    )
+    "codelists/nhsd-primary-care-domain-refsets-dmres_cod.csv", column="code"
+)
 
-# Create dataset
+index_date = "2020-03-31"
+
 dataset = Dataset()
 
-# Create date_of_birth variable from patients table
-dataset.date_of_birth = patients.date_of_birth
+prior_clinical_events = clinical_events.where(
+    clinical_events.date.is_on_or_before(index_date)
+)
 
-# Define study population
-dataset.define_population(patients.date_of_birth.is_on_or_before("1999-12-31"))
+latest_diabetes_event = (
+    prior_clinical_events.where(
+        prior_clinical_events.snomedct_code.is_in(diabetes_codelist)
+    )
+    .sort_by(prior_clinical_events.date)
+    .last_for_patient()
+)
+
+latest_diabetes_resolved_event = (
+    prior_clinical_events.where(
+        prior_clinical_events.snomedct_code.is_in(diabetes_resolved_codelist)
+    )
+    .sort_by(prior_clinical_events.date)
+    .last_for_patient()
+)
+
+dataset.age = patients.age_on(index_date)
+dataset.has_diabetes = latest_diabetes_resolved_event.date < latest_diabetes_event.date
+
+dataset.define_population((dataset.age > 18) & dataset.has_diabetes)
